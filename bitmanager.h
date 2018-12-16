1c21ffcb932723adb5bc1f25a64e1d8dff967c60
unknown <martinfounder@163.com>
2018-12-16 18:28:12 +0800

    I add a bit manager class

---
+++ bitmanager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/***************
+*该类用于管理一段位图信息
+*by mtf
+**/
+class bitmanager
+{
+    public:
+        bitmanager(int length, char *data, int eLength);
+        virtual ~bitmanager();
+        bool atPos(int n);   //取出第n位的值，n从0开始计数
+        int firstZero(int n);   //返回第一个值为0的位的位置
+        bool anyZero();  //返回值标识位图中是否有值为0的位
+        bool setBitmap(int pos, bool value);   //设置位图某位的值
+        int setEffectiveLength(int length);
+
+    protected:
+
+    private:
+        int bmLength;    //位图信息最大长度，该长度以字节为单位
+        //char *bitmap;
+        char *realData;   //实际的位图位置指针
+        int effectiveLength; //有效长度，该长度以位为单位
+};
+
