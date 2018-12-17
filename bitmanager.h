c32f8dcb00446e55ae64ef704b0cb467a8549ede
unknown <martinfounder@163.com>
2018-12-17 19:13:01 +0800

    variables changed

--- bitmanager.h
+++ bitmanager.h
@@ -7,13 +7,14 @@
 class bitmanager
 {
     public:
-        bitmanager(int length, char *data, int eLength);
+        bitmanager(int length, char *data);
         virtual ~bitmanager();
         bool atPos(int n);   //取出第n位的值，n从0开始计数
-        int firstZero(int n);   //返回第一个值为0的位的位置
+        int firstBit(int n, bool val);   //返回第一个值为0的位的位置
         bool anyZero();  //返回值标识位图中是否有值为0的位
         bool setBitmap(int pos, bool value);   //设置位图某位的值
-        int setEffectiveLength(int length);
+        //int setEffectiveLength(int length);
+		void redirectBitmap(int length, char *data);
 
     protected:
 
@@ -21,6 +22,5 @@ class bitmanager
         int bmLength;    //位图信息最大长度，该长度以字节为单位
         //char *bitmap;
         char *realData;   //实际的位图位置指针
-        int effectiveLength; //有效长度，该长度以位为单位
+        //int effectiveLength; //有效长度，该长度以位为单位
 };
-
